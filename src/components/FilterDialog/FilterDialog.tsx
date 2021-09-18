b82d15654bf1a15d4067d3e93ca09ccb0519bce5
Tom Nolan <tsnolan@outlook.com>
2021-09-18 16:30:24 -0400

    chore: wrap filters in Dialog.Content

--- src/components/FilterDialog/FilterDialog.tsx
+++ src/components/FilterDialog/FilterDialog.tsx
@@ -25,24 +25,26 @@ export default function FilterDialog({ isOpen = false, onClose }: Props) {
 
   return (
     <Dialog onClose={onClose} isOpen={isOpen} title="Filter">
-      <form onSubmit={handleSubmit} className="space-y-8">
-        <div>
-          <label
-            className="text-sm text-gray-500 block mb-1"
-            htmlFor="filter--keyword"
-          >
-            By keyword
-          </label>
-          <Input
-            id="filter--keyword"
-            onChange={(e) => setKeyword(e.target.value)}
-            value={keyword}
-          />
-        </div>
-        <Button type="submit" options={{ fluid: true }}>
-          Apply filters
-        </Button>
-      </form>
+      <Dialog.Content>
+        <form onSubmit={handleSubmit} className="space-y-8">
+          <div>
+            <label
+              className="text-sm text-gray-500 block mb-1"
+              htmlFor="filter--keyword"
+            >
+              By keyword
+            </label>
+            <Input
+              id="filter--keyword"
+              onChange={(e) => setKeyword(e.target.value)}
+              value={keyword}
+            />
+          </div>
+          <Button type="submit" options={{ fluid: true }}>
+            Apply filters
+          </Button>
+        </form>
+      </Dialog.Content>
     </Dialog>
   );
 }
