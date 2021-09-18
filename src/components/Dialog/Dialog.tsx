28912be6e0faec3548d2dedce427d28fe24da077
Tom Nolan <tsnolan@outlook.com>
2021-09-18 08:52:09 -0400

    refactor: use @reach/dialog for modal

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,3 +1,4 @@
+import { Dialog as ReachDialog } from "@reach/dialog";
 import Button from "components/Button";
 import CloseIcon from "icons/Close";
 import { ReactNode } from "react";
@@ -15,18 +16,22 @@ export default function Dialog({
   onClose,
   title,
 }: Props) {
-  if (!isOpen) return null;
-
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-white shadow rounded-t-2xl p-4 max-h-96">
-      <div className="flex justify-between font-semibold text-lg text-gray-700 mb-4">
-        {title}
-        <Button onClick={onClose} theme="mutedLink">
-          <CloseIcon />
-          <span className="sr-only">Close</span>
-        </Button>
+    <ReachDialog
+      className="fixed bottom-0 left-0 w-full h-full bg-white"
+      isOpen={isOpen}
+      onDismiss={onClose}
+    >
+      <div className="p-4">
+        <div className="flex justify-between font-semibold text-lg text-gray-700 mb-4">
+          {title}
+          <Button onClick={onClose} theme="mutedLink">
+            <CloseIcon />
+            <span className="sr-only">Close</span>
+          </Button>
+        </div>
+        <div>{children}</div>
       </div>
-      <div>{children}</div>
-    </div>
+    </ReachDialog>
   );
 }
