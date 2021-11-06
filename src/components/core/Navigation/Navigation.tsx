54cb0c65f406950c8a870299964a2996749dc99f
Tom Nolan <tsnolan@outlook.com>
2021-11-05 22:36:46 -0400

    chore: link logo

--- src/components/core/Navigation/Navigation.tsx
+++ src/components/core/Navigation/Navigation.tsx
@@ -6,6 +6,7 @@ import NavLink from "components/core/NavLink";
 import SROnly from "components/core/SROnly";
 import Urls from "constants/urls";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
   const [navIsExpanded, setNavIsExpanded] = useState(false);
@@ -13,7 +14,9 @@ export default function Navigation() {
   return (
     <div className="c-navigation">
       <div className="flex items-center justify-between w-full md:justify-center">
-        <Logo />
+        <Link to={Urls.routes["journal:list"]}>
+          <Logo />
+        </Link>
         <button
           onClick={() => setNavIsExpanded(!navIsExpanded)}
           type="button"
