6bdee1cb467eac029815fd3dfbc10d932a725e81
Tom Nolan <tsnolan@outlook.com>
2021-11-05 21:26:35 -0400

    feat(tip): add action prop

--- src/components/core/Tip/Tip.tsx
+++ src/components/core/Tip/Tip.tsx
@@ -1,15 +1,19 @@
+import { ReactNode } from "react";
+
 type Props = {
   title: string;
   description: string;
+  action?: ReactNode;
 };
 
-export default function Tip({ title, description }: Props) {
+export default function Tip({ action, title, description }: Props) {
   return (
-    <div className="bg-primary-100 rounded-3xl p-6 lg:p-8 space-y-4">
-      <div className="text-lg lg:text-2xl font-semibold text-primary-700">
+    <div className="bg-primary-100 rounded-3xl p-6 lg:p-8">
+      <div className="text-lg lg:text-2xl font-semibold text-primary-700 mb-4">
         {title}
       </div>
       <div className="lg:text-lg text-primary-900 max-w-xl">{description}</div>
+      {action && <div className="mt-8">{action}</div>}
     </div>
   );
 }
