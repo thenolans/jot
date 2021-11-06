17ea9421fccb947e2eaeab748b97a146c50a629e
Tom Nolan <tsnolan@outlook.com>
2021-11-05 21:26:04 -0400

    chore: add Modal.Body

--- src/components/core/ConfirmModal/ConfirmModal.tsx
+++ src/components/core/ConfirmModal/ConfirmModal.tsx
@@ -8,17 +8,19 @@ type Props = Omit<ModalProps, "children"> & {
 export default function ConfirmModal({ title, onConfirm, ...props }: Props) {
   return (
     <Modal hideClose {...props}>
-      <div className="text-center py-2 space-y-6">
-        <div>{title}</div>
-        <div className="space-x-6">
-          <Button onClick={props.onClose} theme="link--muted">
-            Cancel
-          </Button>
-          <Button theme="danger" onClick={onConfirm}>
-            Confirm
-          </Button>
+      <Modal.Body>
+        <div className="text-center py-2 space-y-6">
+          <div>{title}</div>
+          <div className="space-x-6">
+            <Button onClick={props.onClose} theme="link--muted">
+              Cancel
+            </Button>
+            <Button theme="danger" onClick={onConfirm}>
+              Confirm
+            </Button>
+          </div>
         </div>
-      </div>
+      </Modal.Body>
     </Modal>
   );
 }
