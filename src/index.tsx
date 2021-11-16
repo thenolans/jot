17ed52c2faa2120f9093638cbd8e2acde80f7884
Tom Nolan <tsnolan@outlook.com>
2021-11-16 14:25:50 -0500

    feat(pwa): add dependencies for pwa

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from "components/modules/app";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
@@ -13,3 +15,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
