f43e9c89eb53a50e363bf9b3e0a3b8b9494f60b4
Tom Nolan <tsnolan@outlook.com>
2025-07-06 16:12:40 -0400

    feat: increase add button size

--- src/components/AddNoteButton/AddNoteButton.tsx
+++ src/components/AddNoteButton/AddNoteButton.tsx
@@ -17,11 +17,11 @@ export default function AddNoteButton() {
 
   return (
     <Button
-      className="fixed bottom-8 right-8 w-12 h-12 bg-primary-800 hover:bg-primary-900 transition-colors text-white rounded-lg flex items-center justify-center"
+      className="fixed bottom-8 right-8 w-16 h-16 bg-primary-800 hover:bg-primary-900 transition-colors text-white rounded-lg flex items-center justify-center"
       theme="reset"
       onClick={() => handleAdd()}
     >
-      <Icon icon="Plus" />
+      <Icon size={32} icon="Plus" />
       <span className="sr-only">Create new note</span>
     </Button>
   );
