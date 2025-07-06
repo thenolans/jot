d8ff0f518f1473dcd0cbf7b87cfd9b94e390105d
Tom Nolan <tsnolan@outlook.com>
2025-07-06 17:11:08 -0400

    feat: apply basic branding

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,4 @@
 import { AccountDropdown, useSSO } from "@thenolans/nolan-ui";
-import JotLogo from "assets/logo.svg";
 
 export default function NavBar() {
   const { logout } = useSSO();
@@ -7,7 +6,9 @@ export default function NavBar() {
   return (
     <div className="w-full bg-white sticky top-0 z-20 border-b-2 border-gray-100">
       <div className="flex items-center justify-between h-16 max-w-5xl mx-auto px-2 sm:px-4">
-        <img className="h-12" src={JotLogo} alt="Jot" />
+        <h1 className="font-display font-semibold text-4xl text-primary-800 text-center">
+          Jot
+        </h1>
         <AccountDropdown>
           <AccountDropdown.Button onClick={() => logout(window.location.href)}>
             Logout
