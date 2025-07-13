408a1967cc6347189957b98b9a806404e26cd003
daceynolan <daceyshirley@gmail.com>
2025-07-13 18:05:34 -0400

    chore: use nolan-ui search

--- src/components/NoteSearch/NoteSearch.tsx
+++ src/components/NoteSearch/NoteSearch.tsx
@@ -1,4 +1,4 @@
-import { Icon, useDebounce } from "@thenolans/nolan-ui";
+import { SearchInput, useDebounce } from "@thenolans/nolan-ui";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { NotesFilterParams } from "types";
@@ -19,15 +19,13 @@ export default function NoteSearch() {
   }, [debouncedSearchQuery, searchParams, setSearchParams]);
 
   return (
-    <div className="relative sm:max-w-96 focus-within:text-primary-800 text-gray-200 mx-auto">
-      <div className="absolute left-4 top-1/2 -translate-y-1/2">
-        <Icon icon="Search" />
-      </div>
-      <input
+    <div className="sm:max-w-96 mx-auto">
+      <SearchInput
+        name="search"
         placeholder="Search notes..."
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
-        className="border-2 border-transparent shadow h-12 w-full pl-12 mr-auto bg-white rounded-xl sm:rounded-3xl px-4 outline-none focus:border-primary-800 text-gray-700"
+        onClear={() => setSearchQuery("")}
       />
     </div>
   );
