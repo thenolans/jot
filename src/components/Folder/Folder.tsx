4dabe8798f5c244867d80ffde1c292451d4ceb1c
Tom Nolan <tom.nolan@trunk.tools>
2026-02-21 10:35:23 -0500

    chore: ellipses folder name overlfow

--- src/components/Folder/Folder.tsx
+++ src/components/Folder/Folder.tsx
@@ -9,10 +9,10 @@ export default function Folder({ folder }: Props) {
   return (
     <Card canClick>
       <div className="flex items-center">
-        <div className="px-4">
+        <div className="px-4 min-w-0 flex-1">
           <div className="flex items-center space-x-4">
             <Icon className="text-primary-800" icon="Folder" />
-            <div className="text-sm sm:text-base">{folder.name}</div>
+            <div className="text-sm sm:text-base overflow-hidden text-ellipsis whitespace-nowrap">{folder.name}</div>
           </div>
         </div>
         <div className="ml-auto w-12 h-12 text-sm text-primary-800 font-bold bg-primary-50 flex items-center justify-center">
